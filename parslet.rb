b4ab1fbdae3794bf3d82cfaf7faea4fe0552fd80
Paolo Brasolin <paolo@leanbit.it>
2017-06-29 09:27:55 +0200

    progress while traveling

--- parslet.rb
+++ parslet.rb
@@ -4,30 +4,125 @@ require 'yaml'
 class XY < Parslet::Parser
   rule(:matrix) {
     str("\\xymatrix") >>
+      str("nocompile").maybe >>
       str('{') >>
-      cells.as(:row) >>
+      rows.as(:matrix) >>
       str('}')
   }
 
-  rule(:cell_delimiter) { str('&') }
-  rule(:row_delimiter) { str('\\') }
+  rule(:cell) { match['a-z'].repeat.as(:cell) }
+  rule(:cells) do
+    ( cell.maybe >> str('&') ).repeat >>
+    cell.maybe >> ( str('\\') | str('}') ).present?
+  end
+  rule(:row) { cells.as(:row) }
+  rule(:rows) do
+    ( row.maybe >> str('\\') ).repeat >>
+    ( str('}').absent? >> row ).maybe >> str('}').present?
+  end
 
-  rule(:undelimited_cell) { match['a-z'].repeat(1).as(:cell) }
-  rule(:delimited_cell) { undelimited_cell.maybe >> cell_delimiter }
-  rule(:cells) { delimited_cell.repeat >> undelimited_cell.maybe  >> (str('\\') | str('}')).present? }
+  rule(:text) { match['a-zA-Z '].repeat(1) }
+  rule(:space) { str(' ') }
+  rule(:code) do
+    ( macro | group | text ).repeat
+  end
+  rule(:macro) do
+    ( str('\\') >> match['a-zA-Z@'].repeat >> space.repeat ).as(:macro)
+  end
+  rule(:group) { str('{') >> code.maybe.as(:group) >> str('}') }
 
-  # rule(:undelimited_row) { cells.as(:row) }
-  # rule(:delimited_row) { undelimited_row.maybe >> str('\\') }
-  # rule(:rows) { delimited_row.repeat >> undelimited_row.maybe >> str('}').present? }
+  rule(:style) do # NOTE: empty style must be valid
+    str('@{') >>
+      match['=.:>~>-'].repeat.as(:style) >>
+      str('}')
+  end
 
-  root(:matrix)
+  rule(:unit) do # TODO: insert all units
+    str('pt') | str('em') | str('sp') | str('in')
+  end
+
+  rule(:length) do # TODO: check all formats
+    match['0-9'].repeat >>
+      ( str('.') >> match['0-9'].repeat ).maybe >>
+      unit
+  end
+
+  rule(:directions) do # TODO: are directions that rigid?
+    str('@(') >>
+      (
+        ( ( match['ud'] >> match['rl'] ) | match['ud'] | match['rl'] ) >>
+        str(',') >>
+        ( ( match['ud'] >> match['rl'] ) | match['ud'] | match['rl'] )
+      ).as(:directions) >>
+      str(')')
+  end
+
+  rule(:curving) do
+    str('@/') >>
+      (
+        ( str('^') | str('_') ).as(:direction) >>
+        length.maybe.as(:length)
+      ).as(:curving) >>
+      str('/')
+  end
+
+  rule(:hop) do
+    str('[') >>
+      match['urld'].repeat(1).as(:hop) >>
+      str(']')
+  end
+
+  rule(:label) do
+    ( str('^') | str('|') | str('_') ).as(:position) >>
+      ( str('-') ).as(:place).maybe >>
+      code.as(:content)
+  end
+
+  rule(:arrow) do
+    (
+      str('\\ar') >>
+      directions >>
+      style >>
+      hop >>
+      label.as(:label).repeat
+    ).as(:arrow)
+  end
+
+  root(:arrow)
 end
 
 def parse(str)
-  mini = XY.new
-  mini.parse(str)
+  xy = XY.new.parse(str)
+  xy
 rescue Parslet::ParseFailed => failure
   puts failure.parse_failure_cause.ascii_tree
 end
 
-puts parse("\\xymatrix{a&b&c}")
+
+
+puts parse "\\ar@(ul,r)@{=>}[ul]^{wot}_f"
+
+# puts "Undelimited single row"
+
+# puts parse("\\xymatrix{}")
+# puts parse("\\xymatrix{a}")
+# puts parse("\\xymatrix{a&}")
+# puts parse("\\xymatrix{a&b}")
+# puts parse("\\xymatrix{a&b&}")
+# puts parse("\\xymatrix{a&b&c}")
+
+# puts "Delimited single row"
+
+# puts parse("\\xymatrix{\\}")
+# puts parse("\\xymatrix{a\\}")
+# puts parse("\\xymatrix{a&\\}")
+# puts parse("\\xymatrix{a&b\\}")
+# puts parse("\\xymatrix{a&b&\\}")
+# puts parse("\\xymatrix{a&b&c\\}")
+
+# puts "Multiple rows"
+
+# puts parse("\\xymatrix{a\\\\}")
+# puts parse("\\xymatrix{a\\\\\\}")
+# puts parse("\\xymatrix{a\\\\\\&&}")
+# puts parse("\\xymatrix{a&b&c\\d&e&f\\g&h&i\\}")
