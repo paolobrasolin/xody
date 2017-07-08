e26f0a80d9dda9221a1439c6a2af446015e708f2
Paolo Brasolin <paolo@leanbit.it>
2017-07-08 19:18:26 +0200

    folder cleanup

--- lib/xy_parser.rb
+++ lib/xy_parser.rb
@@ -1,6 +1,5 @@
 # coding: utf-8
 require 'parslet'
-require 'yaml'
 
 class XYParser < Parslet::Parser
 
---
+++ lib/xy_transform.rb
@@ -0,0 +1,5 @@
+# coding: utf-8
+require 'parslet'
+
+class XYTransform < Parslet::Transform
+end
